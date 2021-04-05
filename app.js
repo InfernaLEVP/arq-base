f32e1dd2b743d5cc29a11346de4ba64b56b9042d
infernalevp <zozulyaevgeny1995@gmail.com>
2021-04-05 13:22:46 +0300

    Python fix

--- app.js
+++ app.js
@@ -137,7 +137,7 @@ ProtectedRoutes.get('/generate', (req, res) => {
  
     var dataToSend;
     // spawn new child process to call the python script
-    const python = spawn('py', ['ARQUANT_slides_for_Arseny.py']);
+    const python = spawn('python3', ['ARQUANT_slides_for_Arseny.py']);
     // collect data from script
     python.stdout.on('data', function (data) {
         console.log('Pipe data from python script ...');
